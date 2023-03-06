1559b7db765260027a1da540a7baf4316f964a52
nlakhal <nlakhal@ADW-LT-504694.ad.hct.ac.ae>
2023-03-06 21:44:55 +0400

    Generic W4Ex03

--- Activity04_Ex03/src/main/java/ae/hct/week4_userdeginedgenericlassex2/Fridge.java
+++ Activity04_Ex03/src/main/java/ae/hct/week4_userdeginedgenericlassex2/Fridge.java
@@ -6,7 +6,18 @@ package ae.hct.week4_userdeginedgenericlassex2;
  */
 
 /**
- *
+ *what is the difference between      T[] items and Object[] items
+
+In Java, T[] items is a generic array type,
+* where T is a type parameter that is determined at runtime when the class is instantiated. 
+* The type T can represent any class or interface, and the T[] array can only contain elements of type T.
+
+On the other hand, Object[] items is an array of objects, which means it can hold any object regardless of its type. 
+* However, when accessing elements of the Object[] array, 
+* the type of the element must be explicitly casted to its original type before it can be used.
+
+In summary, T[] items provides type safety at compile time and runtime, 
+* while Object[] items is less restrictive but requires casting at runtime to determine the type of the elements.
  */
 public class Fridge<T> {
     private Object[] juice;
